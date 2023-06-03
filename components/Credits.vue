9baf00f906f3be2e9f781155ff5fa452d5eaf945
dpointeck <daniel.pointecker@gmail.com>
2023-06-03 12:39:31 +0200

    Add github link

--- components/Credits.vue
+++ components/Credits.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid justify-center mt-8 text-slate-400">
     <span class="text-xs">Made by 👨‍💻<a href="https://twitter.com/dpointeck">@dpointeck</a></span>
-    <span ><a href="" class=" mt-2 text-xs inline-flex"><GithubLogo /> TimeTrim @github</a></span>
+    <span ><a href="https://github.com/dpointeck/timetrim" class=" mt-2 text-xs inline-flex"><GithubLogo /> TimeTrim @github</a></span>
   </div>
 </template>
 
